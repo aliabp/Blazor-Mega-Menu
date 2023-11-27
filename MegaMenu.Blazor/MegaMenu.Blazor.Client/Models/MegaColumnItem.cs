cbc20db03dd26f6ac72f7486b2a8da50d322d7d1
aliabp <ali.abp@outlook.com>
2023-11-27 15:43:44 +0330

    Add model class for managing menu items.

---
+++ MegaMenu.Blazor/MegaMenu.Blazor.Client/Models/MegaColumnItem.cs
@@ -0,0 +1,16 @@
+﻿namespace Mo.web.Client.Models;
+
+public class MegaColumnItem
+{
+    //[Key]
+    //int Id { get; set; }
+
+    public int ColumnIndex { get; set; }
+
+    public bool IsHeader { get; set; }
+    public bool IsTempDeleted { get; set; }
+
+    public string Text { get; set; }
+
+    public string Link { get; set; }
+}
